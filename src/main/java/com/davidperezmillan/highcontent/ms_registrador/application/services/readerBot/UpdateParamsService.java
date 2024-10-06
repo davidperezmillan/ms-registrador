bdb9f153f3ce20bdfbbc947beee6d66fb9d23606
David Perez Millan <david.perez.millan@gmail.com>
2024-10-06 21:05:05 +0200

    delete duplicate update param

--- src/main/java/com/davidperezmillan/highcontent/ms_registrador/application/services/readerBot/UpdateParamsService.java
+++ src/main/java/com/davidperezmillan/highcontent/ms_registrador/application/services/readerBot/UpdateParamsService.java
@@ -5,6 +5,10 @@ import com.davidperezmillan.highcontent.ms_registrador.domain.model.Param;
 import com.davidperezmillan.highcontent.ms_registrador.domain.usecases.readerBot.UpdateParamsUseCase;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 public class UpdateParamsService implements UpdateParamsUseCase {
 
@@ -16,6 +20,23 @@ public class UpdateParamsService implements UpdateParamsUseCase {
 
     @Override
     public Param updateParam(Param param) {
+        param = deleteDuplicateValues(param);
         return paramsPort.updateParam(param);
+    }
+
+    private Param deleteDuplicateValues(Param param) {
+        String[] values = param.getValue().split(",");
+        String[] uniqueValues = skipDuplicateString(values);
+        param.setValue(convertArrayToString(uniqueValues));
+        return param;
+    }
+
+    private String convertArrayToString(String[] array) {
+        return String.join(",", array);
+    }
+
+    private String[] skipDuplicateString(String[] array) {
+        Set<String> uniqueStrings = new HashSet<>(Arrays.asList(array));
+        return uniqueStrings.toArray(new String[0]);
     }
 }
